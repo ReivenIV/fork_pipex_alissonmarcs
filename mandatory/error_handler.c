24a039a26aa12d09c5b01d39b5144ce9f09818ba
Alisson <alissonmarcos249@gmail.com>
2023-12-12 20:58:57 -0300

    handling invalid arguments

--- mandatory/error_handler.c
+++ mandatory/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: alisson <alisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 15:50:17 by almarcos          #+#    #+#             */
-/*   Updated: 2023/12/12 19:44:06 by alisson          ###   ########.fr       */
+/*   Updated: 2023/12/12 20:57:01 by alisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ void error_handler(t_pipex *pipex, int exit_status, char *msg)
 	}
 	else if (exit_status == 2)
 		ft_putstr_fd("pipex: split function failed\n", 2);
+	else if (exit_status == 3)
+		ft_putstr_fd("Invalid arguments. Take a look in " \
+	"https://github.com/alissonmarcs/pipex for exemples of usage.\n", 2);
 	else if (exit_status == 127)
 	{
 		ft_putstr_fd("pipex: ", 2);
--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: alisson <alisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 13:55:10 by almarcos          #+#    #+#             */
-/*   Updated: 2023/12/12 20:28:09 by alisson          ###   ########.fr       */
+/*   Updated: 2023/12/12 20:42:53 by alisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,15 @@ int	main(int argc, char *argv[], char *env[])
 	t_pipex	pipex;
 	int exit_status;
 
-	init_pipex(&pipex, argc, argv, env);
-	first_child(&pipex, argv[2]);
-	waitpid(pipex.first_child, NULL, 0);
-	second_child(&pipex, argv[3]);
-	close_files(&pipex);
-	waitpid(pipex.second_child, &exit_status, 0);
-	exit(WEXITSTATUS(exit_status));
+	if (argc == 5)
+	{
+		init_pipex(&pipex, argc, argv, env);
+		first_child(&pipex, argv[2]);
+		waitpid(pipex.first_child, NULL, 0);
+		second_child(&pipex, argv[3]);
+		close_files(&pipex);
+		waitpid(pipex.second_child, &exit_status, 0);
+		exit(WEXITSTATUS(exit_status));
+	}
+	error_handler(&pipex, 3, NULL);
 }
